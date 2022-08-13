e3b193b43833423cbf8e1b21372140511f337c5a
gmanricks <contact@lushaylabs.com>
2022-08-13 17:08:23 +0300

    added counter example

---
+++ counter/counter.v
@@ -0,0 +1,20 @@
+module counter
+(
+    input clk,
+    output [5:0] led
+);
+
+localparam WAIT_TIME = 13500000;
+reg [5:0] ledCounter = 0;
+reg [23:0] clockCounter = 0;
+
+always @(posedge clk) begin
+    clockCounter <= clockCounter + 1;
+    if (clockCounter == WAIT_TIME) begin
+        clockCounter <= 0;
+        ledCounter <= ledCounter + 1;
+    end
+end
+
+assign led = ~ledCounter;
+endmodule
