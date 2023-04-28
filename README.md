8a3af408f1d74635650c9eead11d5042fbc94aa0
gmanricks <contact@lushaylabs.com>
2023-04-28 17:48:48 +0300

    added bitstreams

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tang Nano 9K Examples
 
-This repository contains all the example projects for the tang nano 
+This repository contains all the example projects for the tang nano 9k series. All projects tested on OSS CAD Suite version 2023/02/10 which can be downloaded from [here](https://github.com/YosysHQ/oss-cad-suite-build/releases/tag/2023-02-10)
 
 ### Counter
 Introduction project from Part 1 of the TangNano9K series full article can be found [here](https://learn.lushaylabs.com/getting-setup-with-the-tang-nano-9k/)
--- arbiter/top.v
+++ arbiter/top.v
@@ -6,7 +6,24 @@ module toHex(
     output reg [7:0] hexChar = "0"
 );
     always @(posedge clk) begin
-        hexChar <= (value <= 9) ? 8'd48 + value : 8'd55 + value;
+        case (value)
+            0: hexChar <= 8'd48;
+            1: hexChar <= 8'd49;
+            2: hexChar <= 8'd50;
+            3: hexChar <= 8'd51;
+            4: hexChar <= 8'd52;
+            5: hexChar <= 8'd53;
+            6: hexChar <= 8'd54;
+            7: hexChar <= 8'd55;
+            8: hexChar <= 8'd56;
+            9: hexChar <= 8'd57;
+            10: hexChar <= 8'd65;
+            11: hexChar <= 8'd66;
+            12: hexChar <= 8'd67;
+            13: hexChar <= 8'd68;
+            14: hexChar <= 8'd69;
+            15: hexChar <= 8'd70;
+        endcase
     end
 endmodule
 
